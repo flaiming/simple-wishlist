734b35181e4f5954fa90040beaac72d958c686c2
Vojtěch Oram <vojtech@leadspicker.com>
2023-11-25 13:35:57 +0100

    updated Django and other packages

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from wishlist.views import WishlistIntro, WishlistView
 
 urlpatterns = [
-    url(r"^$", WishlistIntro.as_view(), name="wishlist-intro"),
-    url(r"^nove$", WishlistView.as_view(), name="wishlist-create"),
-    url(r"^(?P<slug>[\w\d]+)/$", WishlistView.as_view(), name="wishlist-detail"),
+    re_path(r"^$", WishlistIntro.as_view(), name="wishlist-intro"),
+    re_path(r"^nove$", WishlistView.as_view(), name="wishlist-create"),
+    re_path(r"^(?P<slug>[\w\d]+)/$", WishlistView.as_view(), name="wishlist-detail"),
 ]
